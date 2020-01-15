43a0b4f68b8696515fb283a122ef88f68783b549
Alejandro Carlstein <acarlstein@iqn.com>
2020-01-15 12:01:05 -0700

    AC1 Added Font Awesome and CHANGELOG

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+dom.watch()
+library.add(fas)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <v-container>
       <h1> Home</h1>
+      <font-awesome-icon icon="coffee" />
+      <i class="fas fa-coffee"></i>
   </v-container>
 </template>
 
