1a3ee6d0cd8e9e87f5c0a0760784d3dc0e48846f
Alejandro Carlstein <acarlstein@iqn.com>
2020-01-15 21:26:34 -0700

    AC2 saving changes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <v-app id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <main>
+      <router-view/>
+    </main>
   </v-app>
 </template>
 
 <style lang="scss">
 @import "./scss/App.scss";
+
+#app {
+  min-height: 100vh;
+  height: 100%;
+  width: 100vw;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import HomeView from '../views/Home.vue'
+import PaletteView from '../views/configuration/Palette.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +9,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: HomeView
   },
   {
     path: '/about',
@@ -17,6 +18,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/configuration/palette',
+    name: 'configuration-palette',
+    component: PaletteView
   }
 ]
 
